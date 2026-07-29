d66df25b2acada6dadffb19307e7e9ba8f336c0c
aulnrni <auliajjk31@gmail.com>
2026-07-29 15:48:01 +0700

    nambahin model pada kolom aspirasi

--- app/Models/Aspiration.php
+++ app/Models/Aspiration.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Aspiration extends Model
 {
-    //
+    protected $fillable =[
+        'aspiration_title',
+        'aspiration_content',
+        'category',
+        'submission_date',
+        'aspiration_status',
+    ];
 }
