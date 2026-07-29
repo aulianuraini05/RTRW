94009b2c6c31f51111c22b2267abac9abea53deb
aulnrni <auliajjk31@gmail.com>
2026-07-29 16:17:59 +0700

    menambahkan control pada taabel asset

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -6,5 +6,54 @@ use Illuminate\Http\Request;
 
 class AssetController extends Controller
 {
-    //
+    public function index ()
+    {
+        $assets = Aseet::all();
+        
+        return view('assets.index', compact('assets'));
+    }
+
+    public function create()
+    {
+        return view('assets.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+        'assets_name' => 'required',
+        'assets_type' => 'required',
+        'quantity' => 'required | integer',
+        'condition' => 'required',
+        'description' => 'nullable',
+    ]);
+      Assets::create($request->all());
+    
+      return redirect()->route('assets.index');
+    }
+
+    public function show (Asset $asset)
+    {
+        return view('assets.edit', compact('asset'));
+    }
+
+    public function update(Request $request, Asset $asset)
+    {
+        $request->validate([
+            'asset_name'=> 'required',
+            'asset_type'=>'required',
+            'quantity'=>'required|integer',
+            'condition'=>'required',
+            'description'=>'nullable',
+        ]);
+
+        $asset->update($request->all());
+        return redirect()->route('assets.index');
+    }
+
+    public function destroy(Asset $asset)
+    {
+        $asset->delete();
+        return redirect()->route('assets.index');
+    }
 }
