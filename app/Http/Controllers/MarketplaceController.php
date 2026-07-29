2878adc400b4ecc0c36b391c2fc5d48da441771e
aulnrni <auliajjk31@gmail.com>
2026-07-29 16:32:52 +0700

    nambahin control pada tabel pasar warga

--- app/Http/Controllers/MarketplaceController.php
+++ app/Http/Controllers/MarketplaceController.php
@@ -2,9 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Marketplace;
 use Illuminate\Http\Request;
 
 class MarketplaceController extends Controller
 {
-    //
+    public function index()
+    {
+        $marketplaces = Marketplace::all();
+
+        return view('marketplaces.index', compact('marketplaces'));
+    }
+
+    public function create()
+    {
+        return view('marketplaces.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'product_name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+            'product_status' => 'required',
+        ]);
+
+        Marketplace::create($request->all());
+
+        return redirect()->route('marketplaces.index');
+    }
+
+    public function show(Marketplace $marketplace)
+    {
+        return view('marketplaces.show', compact('marketplace'));
+    }
+
+    public function edit(Marketplace $marketplace)
+    {
+        return view('marketplaces.edit', compact('marketplace'));
+    }
+
+    public function update(Request $request, Marketplace $marketplace)
+    {
+        $request->validate([
+            'product_name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+            'product_status' => 'required',
+        ]);
+
+        $marketplace->update($request->all());
+
+        return redirect()->route('marketplaces.index');
+    }
+
+    public function destroy(Marketplace $marketplace)
+    {
+        $marketplace->delete();
+
+        return redirect()->route('marketplaces.index');
+    }
 }
