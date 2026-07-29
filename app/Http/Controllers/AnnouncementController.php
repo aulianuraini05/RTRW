0d068b6d2386ab1cccc4fe54821b6c61cb0397ba
aulnrni <auliajjk31@gmail.com>
2026-07-29 16:26:43 +0700

    nambahin control pada tabel announcement

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Announcement;
 use Illuminate\Http\Request;
 
 class AnnouncementController extends Controller
 {
-    //
+    public function index()
+    {
+        $announcements = Announcement::all();
+
+        return view('announcements.index', compact('announcements'));
+    }
+
+    public function create()
+    {
+        return view('announcements.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'announcement_title' => 'required',
+            'announcement_content' => 'required',
+            'publication_date' => 'required|date',
+        ]);
+
+        Announcement::create($request->all());
+
+        return redirect()->route('announcements.index');
+    }
+
+    public function show(Announcement $announcement)
+    {
+        return view('announcements.show', compact('announcement'));
+    }
+
+    public function edit(Announcement $announcement)
+    {
+        return view('announcements.edit', compact('announcement'));
+    }
+
+    public function update(Request $request, Announcement $announcement)
+    {
+        $request->validate([
+            'announcement_title' => 'required',
+            'announcement_content' => 'required',
+            'publication_date' => 'required|date',
+        ]);
+
+        $announcement->update($request->all());
+
+        return redirect()->route('announcements.index');
+    }
+
+    public function destroy(Announcement $announcement)
+    {
+        $announcement->delete();
+
+        return redirect()->route('announcements.index');
+    }
 }
