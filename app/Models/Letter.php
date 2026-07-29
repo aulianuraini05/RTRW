c9c4642adbaf6a21ff7c3ba7a4b43526e2e5aa6a
aulnrni <auliajjk31@gmail.com>
2026-07-29 15:28:36 +0700

    menambahkan model letter

--- app/Models/Letter.php
+++ app/Models/Letter.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Letter extends Model
 {
-    //
+    protected $fillable = [
+        'letter_number',
+        'letter_type',
+        'submission_date',
+        'letter_date',
+        'purpose',
+        'letter_status',
+    ]
 }
